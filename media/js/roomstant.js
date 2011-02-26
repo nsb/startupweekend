1670ae605a25b16451080bb1c751e428ebf499f9
Pascal d'Hermilly <pascal@dhermilly.dk>
2011-02-26 21:26:34 +0100

    pop

--- media/js/roomstant.js
+++ media/js/roomstant.js
@@ -3,15 +3,35 @@ jQuery.noConflict();
 (function($, $R){
 
   $(function() {
-
+//     Main suggestion
     var suggestedTemplate = $("#TemplateSuggestedRoom").html();
     var t = _.template(suggestedTemplate);
     if ($R.Rooms.length) {
       var out = t($R.Rooms[0]);
       $("#suggestion").html(out)
     }
-
-    _.each($R.Rooms, console.log);
+    
+    var suggestionListTemplate = $("#TemplateSuggestion_list").html();
+    var t = _.template(suggestionListTemplate);
+    
+    for (var i=0;i<$R.Rooms.length;i++){
+      if (i>3) break;  
+      var out = t($R.Rooms[i]);
+      $("#suggestion_list").append(out);
+    }
+    
   });
 
 })(jQuery, ROOMSTANT);
+
+function priceSort($a,$b){
+    if (a.price<b.price){
+        return -1;
+    } else if (a.price>=b.price){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// function sortRooms{$price,}
