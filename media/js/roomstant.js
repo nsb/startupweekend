5b73746e2c2cfcc2de833820573fa45fdfe18c31
Niels Sandholt Busch <niels.busch@gmail.com>
2011-02-26 20:33:13 +0100

    initial suggested room template

--- media/js/roomstant.js
+++ media/js/roomstant.js
@@ -3,6 +3,12 @@ jQuery.noConflict();
 (function($, $R){
 
   $(function() {
+
+    var suggestedTemplate = $("#TemplateSuggestedRoom").html();
+    var t = _.template(suggestedTemplate);
+    var out = t($R.Rooms[0]);
+    $("#suggestion").html(out)
+
     _.each($R.Rooms, console.log);
   });
 
