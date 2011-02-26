2b1ee8232efa649036ef16a15dae4dbaa6ae95c7
Niels Sandholt Busch <niels.busch@gmail.com>
2011-02-26 13:51:15 +0100

    add slider

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse('<input name="range" type="range" min="0" max="100" step="5" value="15" style="-webkit-appearance: slider-vertical; height: 50px"/>')
