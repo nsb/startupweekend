44d9b71a59bf553087a1561839e5c5b2391e25aa
Niels Sandholt Busch <niels.busch@gmail.com>
2011-02-26 17:02:47 +0100

    add piston handlers

--- roomstant/urls.py
+++ roomstant/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls.defaults import *
 
+from django.views.decorators.csrf import csrf_exempt
+
+from piston.resource import Resource
+from piston.emitters import Emitter
+from piston.authentication import OAuthAuthentication, HttpBasicAuthentication
+
+from roomstant.handlers import HotelHandler, RoomHandler
+
 urlpatterns = patterns('roomstant.views',
 
     (r'^$', 'index', {}, 'index',),
